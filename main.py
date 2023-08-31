382e0ae5422f818b50b115582505fa1c1ba34152
sixingyu <yusx@iastate.edu>
2023-08-31 15:45:21 -0500

    implemented trainer for optimize the model

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from src import load_cifar10, Net
+from src import load_cifar10, Net, training_step
 
 if __name__ == '__main__':
     model = Net()
-    print(model)
+    # print(model)
+    trainloader,testloader,classes = load_cifar10()
+    training_step(model, trainloader, 0)
--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,3 @@
 from .data_handler import load_cifar10
 from .cnn import Net
+from .trainer import training_step
