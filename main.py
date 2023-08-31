c9ef1bd42dd5f9d7883caaee802c63858ffa7f13
sixingyu <yusx@iastate.edu>
2023-08-31 15:51:50 -0500

    main function for training loop

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from src import load_cifar10, Net, training_step,evaluate
 
 if __name__ == '__main__':
+    n_epochs = 10
     model = Net()
-    # print(model)
     trainloader,testloader,classes = load_cifar10()
-    training_step(model, trainloader, 0)
-    evaluate(model, testloader)
+    
+    for epoch in range(n_epochs):
+        training_step(model, trainloader, epoch)
+        evaluate(model, testloader)
+    print("-"*10,"Training finshed","-"*10)
