d2cb2bbd5a926128c0b5b9c3bad57b32ec9f932a
sixingyu <yusx@iastate.edu>
2023-08-31 15:49:36 -0500

    implement evaluate function

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from src import load_cifar10, Net, training_step
+from src import load_cifar10, Net, training_step,evaluate
 
 if __name__ == '__main__':
     model = Net()
     # print(model)
     trainloader,testloader,classes = load_cifar10()
     training_step(model, trainloader, 0)
+    evaluate(model, testloader)
--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,4 @@
 from .data_handler import load_cifar10
 from .cnn import Net
 from .trainer import training_step
+from .eval import evaluate
