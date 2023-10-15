63f414f47fbb817a0bedde52d882561f4195ef7e
Ian Pelakh <ispelakh@iastate.edu>
2023-10-15 14:57:38 -0500

    .

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,5 @@
 from .data_handler import load_cifar10
 from .cnn import Net
-from .cnn2 import ResNet50, ModelParallelResNet50
+from .cnn2 import ResNet50, ModelParallelResNet50, PipelineParallelResNet50
 from .trainer import training_step
 from .eval import evaluate
