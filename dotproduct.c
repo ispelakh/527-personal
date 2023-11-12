f9109fcad84a818092d09becdb13d1721ea4419d
Ian Pelakh <ispelakh@iastate.edu>
2023-11-12 00:50:47 -0600

    dot product

--- dotproduct.c
+++ dotproduct.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /* Define length of dot product vectors */
-#define VECLEN 100
+#define VECLEN 100000
 int main(int argc, char *argv[])
 {
     int i, len = VECLEN;
